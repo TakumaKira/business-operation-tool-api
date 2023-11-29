81d1a91bb4f5e5511b596357103e15fde0a45078
Takuma Kira <takuma@kirakiraworx.com>
2023-11-29 19:21:03 +0900

    [dev] Build GraphQL server

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
+from flask_graphql.graphqlview import GraphQLView
+from .schema import schema
 
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
+    app.add_url_rule("/graphql", view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True))
 
     return app
