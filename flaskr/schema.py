605ed6f495f8abf1fd6b96d86864008cd033b935
Takuma Kira <takuma@kirakiraworx.com>
2023-12-02 16:37:39 +0900

    [dev] Make resolvers resilient and safe

--- flaskr/schema.py
+++ flaskr/schema.py
@@ -1,13 +1,33 @@
 import graphene
 
-from graphene_mongo import MongoengineObjectType
-
 from .models import FakeStoreProduct as FakeStoreProductModel
 
 
-class FakeStoreProduct(MongoengineObjectType):
-    class Meta:
-        model = FakeStoreProductModel
+class FakeStoreProduct(graphene.ObjectType):
+    title = graphene.String()
+    price = graphene.Float()
+    description = graphene.String()
+    category = graphene.String()
+    image = graphene.String()
+    rating = graphene.JSONString()  # Assuming rating is a nested object
+
+    def resolve_title(self, info):
+        return self.data.get('title', None)
+
+    def resolve_price(self, info):
+        return self.data.get('price', None)
+
+    def resolve_description(self, info):
+        return self.data.get('description', None)
+
+    def resolve_category(self, info):
+        return self.data.get('category', None)
+
+    def resolve_image(self, info):
+        return self.data.get('image', None)
+
+    def resolve_rating(self, info):
+        return self.data.get('rating', None)
 
 
 class Query(graphene.ObjectType):
