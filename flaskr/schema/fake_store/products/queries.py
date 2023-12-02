fd0cad2cfba114c1c3b63c8108dfc4839a8f8d06
Takuma Kira <takuma@kirakiraworx.com>
2023-12-02 17:40:50 +0900

    [dev] Get data from external API

--- flaskr/schema/fake_store/products/queries.py
+++ flaskr/schema/fake_store/products/queries.py
@@ -1,9 +1,20 @@
 import graphene
+import requests
 from .type import FakeStoreProductType
 from .model import FakeStoreProductModel
+
 
 class FakeStoreProductQuery(graphene.ObjectType):
     fake_store_products = graphene.List(FakeStoreProductType)
 
     def resolve_fake_store_products(self, info):
-      return list(FakeStoreProductModel.objects.all())
+        if FakeStoreProductModel.objects.count() == 0:
+            print("Get Products data from Fake Store API")
+            response = requests.get("https://fakestoreapi.com/products")
+            products = response.json()
+
+            for product_data in products:
+                product = FakeStoreProductModel(data=product_data)
+                product.save()
+
+        return list(FakeStoreProductModel.objects.all())
