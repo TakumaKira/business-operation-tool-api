e9b84ebc24a52c65e09fa7267846c17771b1b574
Takuma Kira <takuma@kirakiraworx.com>
2023-12-08 17:23:47 +0900

    [refactor] Remove unused import

--- flaskr/database.py
+++ flaskr/database.py
@@ -1,8 +1,6 @@
 from mongoengine import connect
 import mongomock
 
-from .schema.fake_store.products.model import FakeStoreProductModel
-
 connect("business-operation-tool", host="mongodb://localhost", mongo_client_class=mongomock.MongoClient)
 
 
